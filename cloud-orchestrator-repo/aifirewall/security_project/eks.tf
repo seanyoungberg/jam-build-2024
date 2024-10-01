b2b66b93384ad3b95c9e5eabf2c6f50193ba3a3d
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2024-10-02 00:33:00 +0100

    add files

--- cloud-orchestrator-repo/aifirewall/security_project/eks.tf
+++ cloud-orchestrator-repo/aifirewall/security_project/eks.tf
@@ -4,43 +4,43 @@
 
 # SSM, EC2Messages, and SSMMessages endpoints are required for Session Manager
 resource "aws_vpc_endpoint" "spoke1_ssm" {
-  vpc_id     = module.vpc["app1_vpc"].id
-  subnet_ids = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id] ##TODO fix this
-  service_name      = "com.amazonaws.${var.region}.ssm"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = module.vpc["app1_vpc"].id
+  subnet_ids          = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id] ##TODO fix this
+  service_name        = "com.amazonaws.${var.region}.ssm"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
-  security_group_ids = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-ssm-endpoint"})
+  security_group_ids  = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
+  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-ssm-endpoint" })
 }
 
 resource "aws_vpc_endpoint" "spoke1_kms" {
-  vpc_id     = module.vpc["app1_vpc"].id
-  subnet_ids = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id] ##TODO fix this
-  service_name      = "com.amazonaws.${var.region}.kms"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = module.vpc["app1_vpc"].id
+  subnet_ids          = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id] ##TODO fix this
+  service_name        = "com.amazonaws.${var.region}.kms"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
-  security_group_ids = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-kms-endpoint"})
+  security_group_ids  = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
+  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-kms-endpoint" })
 }
 
 resource "aws_vpc_endpoint" "spoke1_ec2messages" {
-  vpc_id     = module.vpc["app1_vpc"].id
-  subnet_ids = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id] ##TODO fix this
-  service_name      = "com.amazonaws.${var.region}.ec2messages"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = module.vpc["app1_vpc"].id
+  subnet_ids          = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id] ##TODO fix this
+  service_name        = "com.amazonaws.${var.region}.ec2messages"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
-  security_group_ids = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-ec2messages-endpoint"})
+  security_group_ids  = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
+  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-ec2messages-endpoint" })
 }
 
 resource "aws_vpc_endpoint" "spoke1_ssmmessages" {
-  vpc_id     = module.vpc["app1_vpc"].id
-  subnet_ids = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id] ##TODO fix this
-  service_name      = "com.amazonaws.${var.region}.ssmmessages"
-  vpc_endpoint_type = "Interface"
+  vpc_id              = module.vpc["app1_vpc"].id
+  subnet_ids          = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id] ##TODO fix this
+  service_name        = "com.amazonaws.${var.region}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
-  security_group_ids = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-ssmmessages-endpoint"})
+  security_group_ids  = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
+  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-ssmmessages-endpoint" })
 }
 
 
@@ -53,7 +53,7 @@ resource "aws_vpc_endpoint" "eks" {
   vpc_endpoint_type  = "Interface"
   subnet_ids         = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id]
   security_group_ids = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-eks-endpoint"})
+  tags               = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-eks-endpoint" })
 }
 
 resource "aws_vpc_endpoint" "ecr_api" {
@@ -62,7 +62,7 @@ resource "aws_vpc_endpoint" "ecr_api" {
   vpc_endpoint_type  = "Interface"
   subnet_ids         = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id]
   security_group_ids = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-ecr-endpoint"})
+  tags               = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-ecr-endpoint" })
 }
 
 resource "aws_vpc_endpoint" "ecr_dkr" {
@@ -71,14 +71,14 @@ resource "aws_vpc_endpoint" "ecr_dkr" {
   vpc_endpoint_type  = "Interface"
   subnet_ids         = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id]
   security_group_ids = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-dkr-endpoint"})
+  tags               = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-dkr-endpoint" })
 }
 
 resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = module.vpc["app1_vpc"].id
-  service_name = "com.amazonaws.${var.region}.s3"
+  vpc_id          = module.vpc["app1_vpc"].id
+  service_name    = "com.amazonaws.${var.region}.s3"
   route_table_ids = [module.subnet_sets["app1_vpc-app1_vm"].unique_route_table_ids["us-east-1a"], module.subnet_sets["app1_vpc-app1_vm"].unique_route_table_ids["us-east-1b"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-s3-endpoint"})
+  tags            = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-s3-endpoint" })
 }
 
 resource "aws_vpc_endpoint" "cloudwatch_logs" {
@@ -87,7 +87,7 @@ resource "aws_vpc_endpoint" "cloudwatch_logs" {
   vpc_endpoint_type  = "Interface"
   subnet_ids         = [module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1a"].id, module.subnet_sets["app1_vpc-app1_vm"].subnets["us-east-1b"].id]
   security_group_ids = [module.vpc["app1_vpc"].security_group_ids["app1_vm"]]
-  tags                = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-logs-endpoint"})
+  tags               = merge(var.global_tags, { "Name" = "${var.name_prefix}spoke1-logs-endpoint" })
 }
 
 ##################################################################
@@ -115,14 +115,14 @@ module "eks_al2023" {
   eks_managed_node_groups = {
     managed_node_group_1 = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      name = "${var.name_prefix}K8s-node"
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m6i.large"]
-      key_name = var.ssh_key_name
-      iam_role_use_name_prefix = true
+      name                                   = "${var.name_prefix}K8s-node"
+      ami_type                               = "AL2023_x86_64_STANDARD"
+      instance_types                         = ["m6i.large"]
+      key_name                               = var.ssh_key_name
+      iam_role_use_name_prefix               = true
       cluster_security_group_use_name_prefix = true
-      iam_role_additional_policies = {SSM = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"}
-      iam_role_tags = var.global_tags
+      iam_role_additional_policies           = { SSM = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore" }
+      iam_role_tags                          = var.global_tags
 
       min_size = 2
       max_size = 2
@@ -132,22 +132,78 @@ module "eks_al2023" {
 
       # This is not required - demonstrates how to pass additional configuration to nodeadm
       # Ref https://awslabs.github.io/amazon-eks-ami/nodeadm/doc/api/
-      # cloudinit_pre_nodeadm = [
-      #   {
-      #     content_type = "application/node.eks.aws"
-      #     content      = <<-EOT
-      #       ---
-      #       apiVersion: node.eks.aws/v1alpha1
-      #       kind: NodeConfig
-      #       spec:
-      #         kubelet:
-      #           config:
-      #             shutdownGracePeriod: 30s
-      #             featureGates:
-      #               DisableKubeletCloudCredentialProviders: true
-      #     EOT
-      #   }
-      # ]
+      cloudinit_pre_nodeadm = [
+        {
+          content_type = "application/node.eks.aws"
+          content      = <<-EOT
+            ---
+            apiVersion: v1
+            kind: Service
+            metadata:
+              name: ui-service
+            spec:
+              type: LoadBalancer
+              ports:
+                - port: 8080
+                  targetPort: 3000
+              selector:
+                app: ui
+            ---
+            apiVersion: apps/v1
+            kind: Deployment
+            metadata:
+              name: ui-deployment
+            spec:
+              replicas: 1
+              selector:
+                matchLabels:
+                  app: ui
+              template:
+                metadata:
+                  labels:
+                    app: ui
+                spec:
+                  containers:
+                    - name: ui
+                      image: migara/ui-app
+                      ports:
+                        - containerPort: 3000
+            ---
+            apiVersion: v1
+            kind: Service
+            metadata:
+              name: agent-service
+            spec:
+              type: ClusterIP
+              ports:
+                - port: 3001
+                  targetPort: 3001
+              selector:
+                app: agent
+            ---
+            apiVersion: apps/v1
+            kind: Deployment
+            metadata:
+              name: agent-deployment
+            spec:
+              replicas: 1
+              selector:
+                matchLabels:
+                  app: agent
+              template:
+                metadata:
+                  labels:
+                    app: agent
+                spec:
+                  containers:
+                    - name: agent
+                      image: migara/agent-app
+                      ports:
+                        - containerPort: 3001
+
+          EOT
+        }
+      ]
     }
   }
 
