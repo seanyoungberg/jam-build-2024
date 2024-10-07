8d18e59e6012416ead0cace6477b2f3612936e43
Sean Youngberg <sean.youngberg@gmail.com>
2024-10-07 04:28:20 -0400

    move

--- security_project/main.tf
+++ security_project/main.tf
@@ -365,7 +365,7 @@ cd /opt/myscripts
 
 # Download scripts from S3
 retry_command aws s3 cp s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/execute_scripts_aws.sh .
-retry_command aws s3 cp s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama3.py
+retry_command aws s3 cp s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama3.py .
 retry_command aws s3 cp s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama3_pj.py .
 retry_command aws s3 cp s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama_pj.py .
 retry_command aws s3 cp s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama_threat.py .
