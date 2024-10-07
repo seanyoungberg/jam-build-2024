add8e803e4ceafffcf447e4700c1fda609e57f4c
Sean Youngberg <sean.youngberg@gmail.com>
2024-10-07 05:04:21 -0400

    still

--- security_project/main.tf
+++ security_project/main.tf
@@ -364,17 +364,25 @@ mkdir -p /opt/myscripts
 cd /opt/myscripts
 
 # Download scripts from S3
-# Download scripts from S3
 retry_command aws s3 cp "s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/execute_scripts_aws.sh" .
 echo "Downloaded execute_scripts_aws.sh: $?"
+
 retry_command aws s3 cp "s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama3.py" .
 echo "Downloaded aws_bedrock_llama3.py: $?"
+
 retry_command aws s3 cp "s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama3_pj.py" .
 echo "Downloaded aws_bedrock_llama3_pj.py: $?"
+
 retry_command aws s3 cp "s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama_pj.py" .
 echo "Downloaded aws_bedrock_llama_pj.py: $?"
+
 retry_command aws s3 cp "s3://panw-aws-jam-challenge-resources-${var.region}/paloalto-ai-runtime-security/aws_bedrock_llama_threat.py" .
 echo "Downloaded aws_bedrock_llama_threat.py: $?"
+
+# List the contents of the directory after downloads
+echo "Contents of /opt/myscripts after downloads:"
+ls -l /opt/myscripts
+
 
 # List the contents of the directory after downloads
 echo "Contents of /opt/myscripts after downloads:"
